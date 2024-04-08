bd672fba1dce224fb42f010f0d192b90906d7fdc
Norbert Skwierczynski <norbert.fe.dev@gmail.com>
2024-04-08 20:54:05 +0100

    changed logo of the project

--- src/components/WorkFlow.jsx
+++ src/components/WorkFlow.jsx
@@ -6,7 +6,7 @@ import { checklistItems } from '../constants';
 const WorkFlow = () => {
   return (
     <div className='mt-20'>
-      <h2 className='text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide'>Accelerate your <span className='bg-gradient-to-r from-violet-500 to-violet-800 text-transparent bg-clip-text'>work experiance.</span></h2>
+      <h2 className='text-3xl sm:text-5xl lg:text-6xl text-center my-20 tracking-wide'>Accelerate your <span className='bg-gradient-to-r from-violet-500 to-violet-800 text-transparent bg-clip-text'>work experience.</span></h2>
       <div className='flex flex-wrap justify-center'>
         <div className='p-2 w-full lg:w-1/2'>
             <img src={codeImg} alt='Code' className='' />
@@ -28,6 +28,6 @@ const WorkFlow = () => {
       </div>
     </div>
   )
-};
+}
 
 export default WorkFlow
