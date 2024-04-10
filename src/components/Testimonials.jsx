67e53ba046223f69fc6946232e988eb9515ed702
norbert skwierczynski <norbert.fe.dev@gmail.com>
2024-04-10 14:50:15 +0100

    Adding comments window for testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -4,7 +4,13 @@ const Testimonials = () => {
   return (
     <div className="mt-20 tracking-wide">
       <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-10 lg:my-20">What parents are saying</h2>
-
+      <div className="flex flex-wrap justify-center">
+        {testimonials.map((testimonial, index) => (
+          <div key={index} className="w-full sm:w-1/2 lg:w-1/3 px-4 py-2">
+            <div className="bg-neutraL"></div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
